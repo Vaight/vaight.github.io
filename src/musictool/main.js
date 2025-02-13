58898faa4a1ed1ae883866eec962c12e99c2dddd
Vaight <109642180+Vaight@users.noreply.github.com>
2025-02-12 18:19:38 -0700

    begin prototyping

--- src/musictool/main.js
+++ src/musictool/main.js
@@ -1,9 +1,35 @@
-let txt = document.getElementById("ctest");
-txt.value = "";
-
-function loadCookie() {
-    txt.value = document.cookie;
+let d1y = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+if (
+    navigator.userAgent.match(/Android/i) || 
+    navigator.userAgent.match(/webOS/i) || 
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
+) {
+    document.getElementById("mnav").className = "mobile";    // format nav bar for mobile devices
+} else {
+    document.getElementById("mnav").className = "desktop";   // format nav bar for desktop devices
 }
-function saveCookie() {
-    document.cookie = txt.value;
+
+function getCookieValue(key) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${key}=`);
+    if (parts.length === 2) { return parts.pop().split(';').shift(); }
+}
+
+function addToCookies(key, value, expiry) {
+    let _c = key + "=" + value + "; expires=" + expiry
+    document.cookie = _c;
+    console.log("adding to cookies: ", _c);
+    let _cs = document.cookie; // get all cookies
+    if (_cs.match(key + "=" + value)) {
+        return true;
+    }
+    return false;
+}
+
+function removeCookie(key) {
+    let _c = key + "=del; expires=Mon, 1 Jan 2000 00:00:00 GMT";
+    document.cookie = _c;
 }
